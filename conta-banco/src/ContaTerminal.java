28812fbecb10d231b6c19708671e381afd841790
Gustavo-Estudos <oliveiragustavo@unitins.br>
2024-05-20 01:22:54 -0300

    ProjetoContaBancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        
+        int numeroConta;
+        String agencia ;
+        String nomeCliente ;
+        double saldo;
+
+        
+        System.out.println("Por favor, digite o número da Conta !");
+        numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente !");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta !");
+        saldo = scanner.nextDouble();
+
+        CriarConta conta1 = new CriarConta(numeroConta, agencia, nomeCliente, saldo);
+
+        System.out.println("Olá " + conta1.getNomeCliente() + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta1.getAgencia() + ", conta " + conta1.getNumeroConta() + " e seu saldo " + conta1.getSaldo() + " já está disponível para saque.");
+
+        System.out.println("Por favor, digite o valor que deseja sacar: ");
+        double sacar = scanner.nextDouble();
+
+        scanner.close();
+
+        conta1.sacarSaldo(sacar);
+                        
     }
 }
---
+++ conta-banco/src/CriarConta.java
@@ -0,0 +1,65 @@
+public class CriarConta {
+
+    private int numeroConta = 0;
+    private String agencia = "";
+    private String nomeCliente = "";
+    private double saldo = 0.0;
+    
+    public CriarConta(int numeroConta, String agencia, String nomeCliente, double saldo) {
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia ) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+       
+
+    public double sacarSaldo(double sacar) {
+        if (sacar > saldo) {
+            System.out.println("Saldo insuficiente!");
+            System.out.println("Seu saldo atual é: " + saldo);
+            return saldo;
+        }
+        else {
+        saldo-= - sacar;
+        System.out.println("Sacando saldo....");
+        System.out.println("Saldo sacado com sucesso!");
+        System.out.println("Você sacou: " + sacar);
+        System.out.println("Seu saldo atual é: " + saldo);      
+        return saldo;
+
+        }
+    }
+    
+}
